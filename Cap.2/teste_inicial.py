378fa21828f19e0f235ff565e8d3cc777f7cfe7a
Gabriel Portela <gabrielportela@ime.eb.br>
2025-03-05 21:13:03 -0400

    atualizacoes

--- Cap.2/teste_inicial.py
+++ Cap.2/teste_inicial.py
@@ -1,8 +1,9 @@
 import os 
 import tarfile 
-import urllib
+import urllib.request
+import pandas
 
-DOWLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+DOWLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/refs/heads/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWLOAD_ROOT + "datasets/housing/housing.tgz"
 
@@ -15,3 +16,12 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz.close()
 
 fetch_housing_data()
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pandas.read_csv(csv_path)
+
+housing = load_housing_data()
+housing.head
+housing.info()
+housing["ocean_proximity"].value_counts()
