a0bff7313c6a117c30a9d8da694090fcd6d07a3b
Gabriel Portela <gabrielportela@ime.eb.br>
2025-03-05 21:59:23 -0400

    ultimas atualizacoes

--- Cap.2/teste_inicial.py
+++ Cap.2/teste_inicial.py
@@ -2,6 +2,8 @@ import os
 import tarfile 
 import urllib.request
 import pandas
+import matplotlib.pyplot as plt
+import numpy as np
 
 DOWLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/refs/heads/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
@@ -15,13 +17,24 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
 
-fetch_housing_data()
-
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pandas.read_csv(csv_path)
 
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+# fetch_housing_data()
 housing = load_housing_data()
-housing.head
-housing.info()
-housing["ocean_proximity"].value_counts()
+# print(housing.head())
+# print(housing.info())
+# print(housing["ocean_proximity"].value_counts())
+# print(housing.describe())
+# housing.hist(bins=50, figsize=(20, 15))
+# plt.show()
+train_set, test_set = split_train_test(housing, 0.2)
+print(len(train_set), "train +", len(test_set), "test")
